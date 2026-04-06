545223732c1cd1e558510239431ed41efdbbd736
Gachiri Mwangi <gachirimwangi24@gmail.com>
2026-04-06 23:47:57 +0300

    Making the site visible across multiple browsers.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { ViteReactSSG } from 'vite-react-ssg'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+export const createRoot = ViteReactSSG(
+  { routes: [{ path: '/', element: <App /> }] }
 )
